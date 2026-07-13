4770cb1a38242a946bcd577bd7f49f6783742478
muttu701957 <Mruthyunjaymarigoudar@gmail.com>
2026-07-13 19:56:29 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ app.use(cookieParser())
 const allowedOrigins = [
   "http://localhost:5173", // local user
   "http://localhost:5174", 
-  "https://app.zeventro.com",
+  "https://app.zeventro.com", // Access with Domain
   "https://www.app.zeventro.com"
 
 
